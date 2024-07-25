724d76f4beff3e3741caa6f6bc81275161dae951
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-25 14:27:23 +0300

    Reworked navbar's routing

--- CrudApp/Models/Thing.cs
+++ CrudApp/Models/Thing.cs
@@ -1,11 +1,13 @@
-﻿namespace CrudApp.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudApp.Models
 {
     public class Thing
     {
         public Guid Id { get; set; }
-
         public string Title { get; set; }
-
         public string? Description { get; set; }
 
 
