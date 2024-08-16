817c4a4c7f26fbe00acc3690148a0e764d65ef5e
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-16 17:38:33 +0300

    Отредактировал модель заменив BsonType с ObjectId на String, чтобы использовать Guid в качестве типа данных для параметра Id, ObjectId генеруется автоматически и представляется в виде строки.Guid в MongoDb не поддерживается. Поэтому мы будем сами создавать Guid для создаваемых в базе объектов. В начал делать репозиторий для работы с базой.

--- CrudApp/Models/MongoThings.cs
+++ CrudApp/Models/MongoThings.cs
@@ -6,11 +6,10 @@ namespace CrudApp.Models
     public class MongoThings
     {
         [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonRepresentation(BsonType.String)]
         public Guid Id { get; set; }
-
         public string Title { get; set; }
         public string Description { get; set; }
-
+            
     }
 }
---
+++ CrudApp/Repositories/MongoThingsRepository.cs
@@ -0,0 +1,47 @@
+﻿using CrudApp.Data;
+using CrudApp.Models;
+using MongoDB.Driver;
+
+namespace CrudApp.Repositories
+{
+    public class MongoThingsRepository
+    {
+        private readonly MongoThingsContext _context;
+
+        public MongoThingsRepository(MongoThingsContext context)
+        {
+           _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<MongoThings>> GetAllAsync()
+        {
+            return await _context.MongoThings.Find(Builders<MongoThings>.Filter.Empty).ToListAsync();
+        }
+
+        public async Task<MongoThings> GetByIdAsync(Guid id)
+        {
+            return await _context.MongoThings.Find(Builders<MongoThings>.Filter.Eq(m => m.Id, id)).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(MongoThings thing)
+        {
+            thing.Id = Guid.NewGuid();
+            await _context.MongoThings.InsertOneAsync(thing);
+        }
+
+        public async void UpdateAsync(Guid id,MongoThings thing)
+        {
+            var filter = Builders<MongoThings>.Filter.Eq(m => m.Id, id);
+
+            var update = Builders<MongoThings>.Update
+                .Set(m => m.Title, thing.Title)
+                .Set(m => m.Description, thing.Description);
+
+            await _context.MongoThings.UpdateOneAsync(filter, update);
+
+        }
+
+
+
+    }
+}
